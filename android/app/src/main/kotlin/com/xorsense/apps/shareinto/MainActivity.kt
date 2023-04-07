8047d9cee7b4c47335ca4ced93ddfe3beef0fd5f
Michael Christenson II <m3talsmith@gmail.com>
2023-04-07 15:32:04 -0400

    Got back to working

---
+++ android/app/src/main/kotlin/com/xorsense/apps/shareinto/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.xorsense.apps.shareinto
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- lib/sharing/sharing_page.dart
+++ lib/sharing/sharing_page.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:path_provider/path_provider.dart';
 
 class SharingPage extends StatefulWidget {
   late String sharedUrl;
@@ -13,34 +16,58 @@ class SharingPageState extends State<SharingPage> {
   String searchTerm = '';
   List<String> paths = [];
 
+  Future<List<String>> getPaths() async {
+    Directory baseDir = await getApplicationDocumentsDirectory();
+    return baseDir.listSync().map((e) => e.path).toList();
+  }
+
+  List<Widget> mapPaths(List<String> paths) =>
+      paths.map((e) => Text(e)).toList();
+
   @override
   Widget build(BuildContext context) {
     return ListView(children: [
-      const Text('Share', textScaleFactor: 1.25,),
+      const Text(
+        'Share Markdown',
+        textScaleFactor: 1.25,
+      ),
       TextFormField(
-        initialValue: widget.sharedUrl,
+        initialValue: '- ${widget.sharedUrl}',
       ),
       Text('Shared Url: ${widget.sharedUrl}'),
       const Divider(),
       const Text('Append To', textScaleFactor: 1.25),
       TextField(
         autofocus: true,
-        onChanged: (String value) {},
+        onChanged: (String value) {
+          setState(() {
+            searchTerm = value;
+          });
+        },
       ),
-      Column(children: paths.map((e) => Text(e)).toList()),
+      FutureBuilder(
+          future: getPaths(),
+          builder:
+              (BuildContext context, AsyncSnapshot<List<String>> snapshot) {
+            List<Widget> children;
+            if (snapshot.hasData) {
+              children = mapPaths(snapshot.data!);
+            } else {
+              children = mapPaths(paths);
+            }
+            return Column(children: children);
+          }),
       Padding(
-        padding: const EdgeInsets.all(10.0),
-        child: ElevatedButton(
-            onPressed: () {},
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: const [
-                Icon(Icons.file_upload_rounded),
-                Text('Create a New File')
-              ],
-            )
-          )
-        ),
+          padding: const EdgeInsets.all(10.0),
+          child: ElevatedButton(
+              onPressed: () {},
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Icon(Icons.file_upload_rounded),
+                  Text('Create $searchTerm.md')
+                ],
+              ))),
     ]);
   }
 }
